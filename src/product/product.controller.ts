f828a061e1d2848c766be87ac4ba76d08a685639
Godwin Onyeka <smarttin77@gmail.com>
2021-09-24 22:34:24 +0100

    product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { ProductCreateDto } from './dtos/product-create.dto';
+import { ProductService } from './product.service';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  // @UseGuards(AuthGuard)
+  @Get('admin/products')
+  async all() {
+    return this.productService.find();
+  }
+
+  // @UseGuards(AuthGuard)
+  @Post('admin/products')
+  async create(@Body() body: ProductCreateDto) {
+    const product = await this.productService.save(body);
+    return product;
+  }
+
+  // @UseGuards(AuthGuard)
+  @Get('admin/products/:id')
+  async get(@Param('id') id: number) {
+    return this.productService.findOne({ id });
+  }
+
+  // @UseGuards(AuthGuard)
+  @Put('admin/products/:id')
+  async update(@Param('id') id: number, @Body() body: ProductCreateDto) {
+    await this.productService.update(id, body);
+    return this.productService.findOne({ id });
+  }
+
+  // @UseGuards(AuthGuard)
+  @Delete('admin/products/:id')
+  async delete(@Param('id') id: number) {
+    const response = await this.productService.delete(id);
+    return response;
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,27 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AbstractService } from 'src/shared/abstract.service';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 
 @Injectable()
-export class UserService {
+export class UserService extends AbstractService {
   constructor(
-    @InjectRepository(User) private readonly userReposiroty: Repository<User>,
-  ) {}
-
-  async save(options) {
-    return this.userReposiroty.save(options);
-  }
-
-  async find(options) {
-    return this.userReposiroty.find(options);
-  }
-
-  async findOne(options) {
-    return this.userReposiroty.findOne(options);
-  }
-
-  async update(id, options) {
-    return this.userReposiroty.update(id, options);
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {
+    super(userRepository);
   }
 }
